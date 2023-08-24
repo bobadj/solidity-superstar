b5ccb5691ffbf3f70eb86b89493ae8e3c9fe0655
bobadj <slobodan.dj2@gmail.com>
2023-08-24 17:49:51 +0200

    Task 2 - Refactor

--- Task 2/test/MetadataExtractor.ts
+++ Task 2/test/MetadataExtractor.ts
@@ -1,20 +1,22 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
+import { MetadataExtractor, MetadataExtractor__factory } from "../typechain-types";
+
+const NFT_METADATA: any = '0x0000000000000001018000ffd8da6bf26964af9d7eed9e03e53415d37aa96045';
+const EXPECTED_RESULT: string = '0x8000ff';
 
 describe('Metadata Extractor Challenge', function () {
-    async function deployMetadataExtractor() {
-        const MetadataExtractor = await ethers.getContractFactory('MetadataExtractor');
-        const metadataExtractor =  await MetadataExtractor.deploy();
+    async function deployMetadataExtractor(): Promise<MetadataExtractor> {
+        const MetadataExtractor: MetadataExtractor__factory = await ethers.getContractFactory('MetadataExtractor');
+        const metadataExtractor: MetadataExtractor =  await MetadataExtractor.deploy();
         await metadataExtractor.waitForDeployment();
         return metadataExtractor;
     }
 
     it('The extractColor() must return proper color hex', async function () {
-        const metadataExtractor = await loadFixture(deployMetadataExtractor);
+        const metadataExtractor: MetadataExtractor = await loadFixture(deployMetadataExtractor);
 
-        expect(
-            await metadataExtractor.extractColor('0x0000000000000001018000ffd8da6bf26964af9d7eed9e03e53415d37aa96045')
-        ).to.be.eq('0x8000ff');
+        expect(await metadataExtractor.extractColor(NFT_METADATA)).to.be.eq(EXPECTED_RESULT);
     });
 });
